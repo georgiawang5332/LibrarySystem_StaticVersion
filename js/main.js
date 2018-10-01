a6d6523ec2075d8c21227d24223f47def22c8775
tiroLinda963 <tiroLinda963@outlook.com>
2018-10-01 14:05:02 +0800

    commit first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**********************************
+*01 : Menu Toggle Script 側邊欄位
+***********************************/
+// $("#menu-toggle").click(function (e) {
+//     e.preventDefault();
+//     $("#main").toggleClass("toggled");
+// });
+/**********************************
+*01 : 捐書 Tooltips (top) 刪除 修改btn
+***********************************/
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
